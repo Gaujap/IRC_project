6c185233cd002e6e8949b029afa6065ff1944fda
Gaujap <debarnot.gabriel@gmail.com>
2025-02-08 16:27:47 +0100

    Add necessary file

---
+++ irc-front/src/Chat.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import './Chat.css';
+
+const socket = io('https://irc-wzmf.onrender.com');
+
+const Chat: React.FC = () => {
+
+};
+
+export default Chat;
