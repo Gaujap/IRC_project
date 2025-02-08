1a5fe65b4787c44590c4cf777a9611cbd94e2f92
Gaujap <debarnot.gabriel@gmail.com>
2025-02-08 17:09:29 +0100

    Add style

--- irc-front/src/Chat.tsx
+++ irc-front/src/Chat.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import './Chat.css';
+import './style/Chat.css';
 
 const socket = io('https://irc-wzmf.onrender.com');
 
