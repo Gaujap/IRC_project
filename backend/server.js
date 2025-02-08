ed9a9e92f32c95105dccca41f63594024fa1bdd8
Gaujap <debarnot.gabriel@gmail.com>
2025-02-08 13:33:04 +0100

    Add cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { Server } = require('socket.io');
 const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://irc-front.onrender.com",
+        methods: ["GET", "POST"]
+    }
+});
 
+app.use(cors());
 app.use(express.json());
 
 server.listen(5000, () => console.log('Server running on port 5000'));
