7cffb62644c0e2214c29402897f37846aff073fa
Gaujap <debarnot.gabriel@gmail.com>
2025-02-08 13:33:32 +0100

    Add mongoDB connection in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const http = require('http');
@@ -15,5 +16,9 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 server.listen(5000, () => console.log('Server running on port 5000'));
