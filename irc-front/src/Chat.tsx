38e7c7a60f79b176056a8114fdbd53c21c64c8fb
Gaujap <debarnot.gabriel@gmail.com>
2025-02-08 16:31:49 +0100

    Add useEffect

--- irc-front/src/Chat.tsx
+++ irc-front/src/Chat.tsx
@@ -5,7 +5,25 @@ import './Chat.css';
 const socket = io('https://irc-wzmf.onrender.com');
 
 const Chat: React.FC = () => {
+    const [messages, setMessages] = useState<{ text: string; pseudo: string}[]>([]);
+    const [pseudo, setPseudo] = useState<string>('');
+    const [isPseudoSet, setIsPseudoSet] = useState<boolean>(false);
+    const messagesEndRef = React.useRef<HTMLDivElement>(null);
 
+    useEffect(() => {
+        const savedPseudo = localStorage.getItem('pseudo');
+        if (savedPseudo) {
+            setPseudo(savedPseudo);
+            setIsPseudoSet(true);
+            socket.emit('setPseudo', savedPseudo);
+        }
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages]);
+
+    return (
+    );
 };
 
 export default Chat;
